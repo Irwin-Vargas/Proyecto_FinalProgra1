eb94d5ea953b6d800b809217c6e3961f0bd90cb7
Irwin-Vargas <irwin_vilca@usmp.pe>
2025-05-22 22:27:44 -0500

    Migraciones listas para Render

--- Migrations/20250523032417_InitRender.Designer.cs
+++ Migrations/20250523032417_InitRender.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Proyecto_FinalProgra1.Data;
@@ -11,9 +12,11 @@ using Proyecto_FinalProgra1.Data;
 namespace Proyecto_FinalProgra1.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250523032417_InitRender")]
+    partial class InitRender
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
