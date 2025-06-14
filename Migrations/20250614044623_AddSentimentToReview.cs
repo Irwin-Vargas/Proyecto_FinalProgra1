ae1947ea7bdb33237e29590b03e21800eb4a4737
Irwin-Vargas <irwin_vilca@usmp.pe>
2025-06-13 23:47:18 -0500

    Migración: Agrega columnas de sentimiento a la tabla Review

---
+++ Migrations/20250614044623_AddSentimentToReview.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Proyecto_FinalProgra1.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSentimentToReview : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "SentimentPositive",
+                table: "Reviews",
+                type: "boolean",
+                nullable: true);
+
+            migrationBuilder.AddColumn<float>(
+                name: "SentimentProbability",
+                table: "Reviews",
+                type: "real",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SentimentPositive",
+                table: "Reviews");
+
+            migrationBuilder.DropColumn(
+                name: "SentimentProbability",
+                table: "Reviews");
+        }
+    }
+}
